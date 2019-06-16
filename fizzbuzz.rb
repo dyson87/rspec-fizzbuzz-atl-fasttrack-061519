9e4e6ead599e8e6703dfd4697a522abe09c345dd
Courtney Dyson <courtneydyson87@gmail.com>
2019-06-16 15:38:27 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
 
 def fizzbuzz(num)
   if num % 3 == 0
-    puts "Fizz"
+    return "Fizz"
   elsif num % 5 == 0
-    puts "Buzz"
+    return "Buzz"
     elsif num % 3 && 5 == 0
-      puts "Fizzbuzz"
+      return "Fizzbuzz"
   else
     nil
   end
